378ed890bde16ec4be2d4e3d66017ad42b0e49cc
Jay <57915168+JayTheferret@users.noreply.github.com>
2020-01-05 21:40:20 +0100

    initial commit

--- GIP_11/INF-11.01-Make/main.h
+++ GIP_11/INF-11.01-Make/main.h
@@ -6,4 +6,5 @@
 cmd -> 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 cd D:\Schule\GIP\GIP_Praktikums-Aufgaben\GIP_11\INF-11.01-Make
+D:
 */
